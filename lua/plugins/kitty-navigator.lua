960abb1abd1bf8777d1f90412ef61319fea486e5
rootiest <chris@rootiest.com>
2024-07-10 04:46:54 -0400

    🗑️ Cleanup and reorganize
    - Remove unused junk and slim down the config

--- lua/plugins/kitty-navigator.lua
+++ lua/plugins/kitty-navigator.lua
@@ -7,4 +7,10 @@ return {
     local term = os.getenv("TERM")
     return term and string.find(term, "kitty")
   end,
+  keys = {
+    { "<c-h>", "<cmd>KittyNavigateLeft<cr>", desc = "KittyNavigateLeft" },
+    { "<c-j>", "<cmd>KittyNavigateDown<cr>", desc = "KittyNavigateDown" },
+    { "<c-k>", "<cmd>KittyNavigateUp<cr>", desc = "KittyNavigateUp" },
+    { "<c-l>", "<cmd>KittyNavigateRight<cr>", desc = "KittyNavigateRight" },
+  },
 }
