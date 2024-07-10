5ac6378b5fe96252c2413fc41a7fef73695861d3
rootiest <chris@rootiest.com>
2024-07-10 13:28:29 -0400

    📱 Remote Session
    - Changed keymap for Remote Session quicklaunch on dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -3,17 +3,35 @@ return {
   lazy = false,
   opts = function()
     if vim.fn.winheight(0) >= 80 and vim.fn.winwidth(0) >= 80 then
-      LOGO = table.concat(vim.fn.readfile(vim.fn.stdpath("config") .. "/logo/rootiest.txt"), "\n")
+      LOGO = table.concat(
+        vim.fn.readfile(vim.fn.stdpath("config") .. "/logo/rootiest.txt"),
+        "\n"
+      )
     elseif vim.fn.winheight(0) >= 48 and vim.fn.winwidth(0) >= 48 then
-      LOGO = table.concat(vim.fn.readfile(vim.fn.stdpath("config") .. "/logo/nvim.txt"), "\n")
+      LOGO = table.concat(
+        vim.fn.readfile(vim.fn.stdpath("config") .. "/logo/nvim.txt"),
+        "\n"
+      )
     elseif vim.fn.winwidth(0) >= 100 then
-      LOGO = table.concat(vim.fn.readfile(vim.fn.stdpath("config") .. "/logo/long.txt"), "\n")
+      LOGO = table.concat(
+        vim.fn.readfile(vim.fn.stdpath("config") .. "/logo/long.txt"),
+        "\n"
+      )
     elseif vim.fn.winwidth(0) >= 80 then
-      LOGO = table.concat(vim.fn.readfile(vim.fn.stdpath("config") .. "/logo/tall.txt"), "\n")
+      LOGO = table.concat(
+        vim.fn.readfile(vim.fn.stdpath("config") .. "/logo/tall.txt"),
+        "\n"
+      )
     elseif vim.fn.winwidth(0) >= 50 then
-      LOGO = table.concat(vim.fn.readfile(vim.fn.stdpath("config") .. "/logo/small.txt"), "\n")
+      LOGO = table.concat(
+        vim.fn.readfile(vim.fn.stdpath("config") .. "/logo/small.txt"),
+        "\n"
+      )
     else
-      LOGO = table.concat(vim.fn.readfile(vim.fn.stdpath("config") .. "/logo/tiny.txt"), "\n")
+      LOGO = table.concat(
+        vim.fn.readfile(vim.fn.stdpath("config") .. "/logo/tiny.txt"),
+        "\n"
+      )
     end
     LOGO = "\n\n" .. LOGO .. "\n\n"
     local opts = {
@@ -32,7 +50,7 @@ return {
             { action = "LazyGit",                                        desc = " LazyGit",         icon = " ", key = "G" },
             { action = 'lua LazyVim.pick.config_files()()',              desc = " Config",          icon = " ", key = "c" },
             { action = 'lua require("persistence").load()',              desc = " Restore Session", icon = " ", key = "s" },
-            { action = 'RemoteStart',                                    desc = " Remote Session",  icon = "󰢹 ", key = "v" },
+            { action = 'RemoteStart',                                    desc = " Remote Session",  icon = "󰢹 ", key = "S" },
             { action = "LazyExtras",                                     desc = " Lazy Extras",     icon = " ", key = "x" },
             { action = "Lazy",                                           desc = " Lazy",            icon = "󰒲 ", key = "l" },
             { action = function() vim.api.nvim_input("<cmd>qa<cr>") end, desc = " Quit",            icon = " ", key = "q" },
