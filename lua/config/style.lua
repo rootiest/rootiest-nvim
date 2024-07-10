95524e7f4e29d7d71b8da9e30bc480a3b87839a0
rootiest <chris@rootiest.com>
2024-07-10 13:20:04 -0400

    🎨 Style Update
    - Change gui font to Iosevka
    - Fix Kitty-theme function

--- lua/config/style.lua
+++ lua/config/style.lua
@@ -4,14 +4,20 @@
 
 -- Set the palette
 if os.getenv("CATPPUCCIN_PALETTE") then
-  TMEMEPALETTE = require("catppuccin.palettes").get_palette(os.getenv("CATPPUCCIN_PALETTE"))
+  TMEMEPALETTE =
+    require("catppuccin.palettes").get_palette(os.getenv("CATPPUCCIN_PALETTE"))
+end
+
+if os.getenv("KITTY_THEME") then
+  KITTY_THEME = os.getenv("KITTY_THEME")
 end
 
 -- Apply the colorscheme
-vim.cmd.colorscheme(kitty_theme or "catppuccin-frappe")
+vim.cmd.colorscheme(KITTY_THEME or "catppuccin-frappe")
 
 -- Set GUI font
-vim.opt.guifont = "MonaspiceKr Nerd Font Mono:#e-subpixelantialias:h13"
+--vim.opt.guifont = "MonaspiceKr Nerd Font Mono:#e-subpixelantialias:h13"
+vim.opt.guifont = "Iosevka:#e-subpixelantialias:h13"
 vim.g.have_nerd_font = true
 
 -- Set transparency
