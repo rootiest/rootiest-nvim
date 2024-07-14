bf1be126fad9791c4919fe0a314f584c0f2477f1
rootiest <chris@rootiest.com>
2024-07-14 00:38:52 -0400

    🎨 Automatic Colorscheme
    - Store/restore the colorscheme when changed
    - Falls back on $KITTY_THEME and then the default catppuccin-frappe

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -9,6 +9,19 @@ vim.api.nvim_create_user_command("Q", function()
   vim.cmd.qall()
 end, { force = true })
 
+-- ------------------------------ Auto-Commands --------------------------------
+-- Autosave Colorscheme
+vim.api.nvim_create_autocmd("ColorScheme", {
+  desc = "Store colorscheme name",
+  callback = function()
+    print("colorscheme:", vim.g.colors_name)
+    vim.fn.writefile(
+      { vim.g.colors_name },
+      vim.fn.stdpath("config") .. "/.colorscheme"
+    )
+  end,
+})
+
 -- --------------------------------- Keymaps -----------------------------------
 local wk = require("which-key")
 wk.add({
--- lua/config/style.lua
+++ lua/config/style.lua
@@ -2,13 +2,19 @@
 -- ---------------------------------- Style ------------------------------------
 -- -----------------------------------------------------------------------------
 
--- Get the kitty theme
-if os.getenv("KITTY_THEME") then
-  KITTY_THEME = os.getenv("KITTY_THEME")
+-- Restore Colorscheme
+if vim.fn.filereadable(vim.fn.stdpath("config") .. "/.colorscheme") == 1 then
+  -- Load the stored colorscheme
+  STORED_THEME = vim.fn.readfile(vim.fn.stdpath("config") .. "/.colorscheme")[1]
+else
+  -- Get the kitty theme
+  if os.getenv("KITTY_THEME") then
+    KITTY_THEME = os.getenv("KITTY_THEME")
+  end
 end
 
 -- Apply the colorscheme
-vim.cmd.colorscheme(KITTY_THEME or "catppuccin-frappe")
+vim.cmd.colorscheme(STORED_THEME or KITTY_THEME or "catppuccin-frappe")
 
 -- Set GUI font
 vim.opt.guifont = "Iosevka:#e-subpixelantialias:h12"
