11b4110201b3d8292a871a1d08f718bb1b863e04
rootiest <chris@rootiest.com>
2024-07-16 11:04:44 -0400

    Theme hlchunk
    - Implement various themes for hkchunk

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -18,15 +18,38 @@ return {
   },
   {
     "shellRaining/hlchunk.nvim",
+    event = "BufEnter",
     config = function()
       local cb = function()
         if vim.g.colors_name == "tokyonight" then
           return "#806d9c"
         elseif vim.g.colors_name:find("catppuccin") then
           local catpalette = require("catppuccin.palettes").get_palette()
-          return catpalette.rosewater
+          return catpalette.green
+        elseif vim.g.colors_name == "monochrome" then
+          return "#CCCCCC"
+        elseif vim.g.colors_name == "gruvbox" then
+          return "#a9b665"
+        elseif vim.g.colors_name == "dracula" then
+          return "#50fa7b"
+        elseif vim.g.colors_name == "onedark" then
+          return "#98C379"
+        elseif vim.g.colors_name == "nightfox" then
+          return "#98C379"
+        elseif vim.g.colors_name == "nord" then
+          return "#81A1C1"
+        elseif vim.g.colors_name == "kanagawa" then
+          return "#73C8AD"
+        elseif vim.g.colors_name == "everforest" then
+          return "#8FBCBB"
+        elseif vim.g.colors_name == "github_dark" then
+          return "#FFFFFF"
+        elseif vim.g.colors_name == "github_light" then
+          return "#3f3f3f"
+        elseif vim.g.colors_name == "neofusion" then
+          return "#420ADD"
         else
-          return "#90FDA9"
+          return "#7581FF"
         end
       end
       require("hlchunk").setup({
@@ -37,7 +60,13 @@ return {
           enable = true,
         },
         indent = {
-          enable = true,
+          enable = function()
+            if vim.g.colors_name == "monochrome" then
+              return false
+            else
+              return true
+            end
+          end,
         },
       })
     end,
