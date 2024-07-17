8f3df3e569eb5c1bc0555b6d3356f5ad8df5b7f2
rootiest <chris@rootiest.com>
2024-07-17 05:52:58 -0400

    🧙 Completion
    - Add additional completion sources:
      - yanky
      - env
      - calc
      - conventionalcommits
    - Add more friendly completion mappings

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -56,55 +56,35 @@ return {
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
       table.insert(opts.sources, { name = "emoji" })
-    end,
-    dependencies = { "hrsh7th/cmp-emoji" },
-  },
-  { -- Supertab completion
-    -- Use <tab> for completion and snippets
-    "hrsh7th/nvim-cmp",
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0
-          and vim.api
-              .nvim_buf_get_lines(0, line - 1, line, true)[1]
-              :sub(col, col)
-              :match("%s")
-            == nil
-      end
-
+      table.insert(opts.sources, { name = "cmp_yanky" })
+      table.insert(opts.sources, { name = "dotenv" })
+      table.insert(opts.sources, { name = "calc" })
+      table.insert(opts.sources, { name = "conventionalcommits" })
       local cmp = require("cmp")
-
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
-            cmp.select_next_item()
-          elseif vim.snippet.active({ direction = 1 }) then
-            vim.schedule(function()
-              vim.snippet.jump(1)
-            end)
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif vim.snippet.active({ direction = -1 }) then
-            vim.schedule(function()
-              vim.snippet.jump(-1)
-            end)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
+      cmp.setup({
+        mapping = {
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<C-S-f>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.close(),
+          ["<CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Insert,
+            select = true,
+          }),
+        },
       })
     end,
+    dependencies = {
+      "hrsh7th/cmp-emoji",
+      "chrisgrieser/cmp_yanky",
+      "SergioRibera/cmp-dotenv",
+      "hrsh7th/cmp-calc",
+      "davidsierradz/cmp-conventionalcommits",
+    },
   },
   {
     "norcalli/nvim-colorizer.lua",
