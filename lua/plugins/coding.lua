02bc1c9d542a3e2c331bb1808fc05cb2c780370b
rootiest <chris@rootiest.com>
2024-07-19 20:43:38 -0400

    🎨 Fancify hlchunck
    - Add fancy glyphs and colors
    - Reduce visibility and auto-dark the colors
    - Reduces distraction while still providing information
    
    - Make cmp-yanky only use history from the same filetyle

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -76,7 +76,12 @@ return {
     end,
     dependencies = {
       "hrsh7th/cmp-emoji",
-      "chrisgrieser/cmp_yanky",
+      {
+        "chrisgrieser/cmp_yanky",
+        option = {
+          onlyCurrentFiletype = false,
+        },
+      },
       "SergioRibera/cmp-dotenv",
       "hrsh7th/cmp-calc",
       "davidsierradz/cmp-conventionalcommits",
@@ -100,64 +105,5 @@ return {
         enabled = false,
       },
     },
-  },
-  {
-    "shellRaining/hlchunk.nvim",
-    event = "BufEnter",
-    config = function()
-      local cb = function()
-        if vim.g.colors_name == "tokyonight" then
-          return "#806d9c"
-        elseif vim.g.colors_name:find("catppuccin") then
-          local catpalette = require("catppuccin.palettes").get_palette()
-          return catpalette.green
-        elseif vim.g.colors_name == "monochrome" then
-          return "#CCCCCC"
-        elseif vim.g.colors_name == "gruvbox" then
-          return "#a9b665"
-        elseif vim.g.colors_name == "dracula" then
-          return "#50fa7b"
-        elseif vim.g.colors_name == "onedark" then
-          return "#98C379"
-        elseif vim.g.colors_name == "nightfox" then
-          return "#98C379"
-        elseif vim.g.colors_name == "nord" then
-          return "#81A1C1"
-        elseif vim.g.colors_name == "kanagawa" then
-          return "#73C8AD"
-        elseif vim.g.colors_name == "everforest" then
-          return "#8FBCBB"
-        elseif vim.g.colors_name == "github_dark" then
-          return "#FFFFFF"
-        elseif vim.g.colors_name == "github_light" then
-          return "#3f3f3f"
-        elseif vim.g.colors_name == "neofusion" then
-          return "#420ADD"
-        else
-          return "#7581FF"
-        end
-      end
-      require("hlchunk").setup({
-        chunk = {
-          style = {
-            { fg = cb },
-          },
-          enable = true,
-        },
-        indent = {
-          enable = function()
-            if vim.g.colors_name == "monochrome" then
-              return false
-            else
-              return true
-            end
-          end,
-        },
-      })
-    end,
-  },
-  {
-    "ton/vim-alternate",
-    event = "VeryLazy",
   },
 }
