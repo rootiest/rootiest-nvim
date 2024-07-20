2ff69ae0a7e8d0b27bf0ad81cf60eecf2c2309d9
rootiest <chris@rootiest.com>
2024-07-19 20:41:23 -0400

    💬 Languages
    - Add cmake, docker, java
    - Tweak markdown-preview build command

--- lua/plugins/languages.lua
+++ lua/plugins/languages.lua
@@ -9,6 +9,16 @@ return {
   { import = "lazyvim.plugins.extras.lang.git" },
   { import = "lazyvim.plugins.extras.lang.python" },
   { import = "lazyvim.plugins.extras.lang.toml" },
+  {
+    "iamcco/markdown-preview.nvim",
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+  },
   { import = "lazyvim.plugins.extras.lang.yaml" },
   { import = "lazyvim.plugins.extras.lang.clangd" },
+  { import = "lazyvim.plugins.extras.lang.cmake" },
+  { import = "lazyvim.plugins.extras.lang.docker" },
+  { import = "lazyvim.plugins.extras.lang.java" },
 }
