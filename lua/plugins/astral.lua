7c856aaaa5e9c108aaa8f8c794b45d078718e69b
rootiest <chris@rootiest.com>
2024-08-02 09:26:51 -0400

    🧹 chore(astral): inconsequential testing changes in astral plugin

--- lua/plugins/astral.lua
+++ lua/plugins/astral.lua
@@ -1,15 +1,17 @@
 return {
-  "rootiest/astral.nvim",
-  version = "*", -- Pin to GitHub releases
-  opts = {
-    fallback_themes = {
-      "catppuccin-frappe",
-      "catppuccin-macchiato",
-      "tokyonight",
-      "kanagawa",
-      "monochrome",
-      "default",
+  {
+    "rootiest/astral.nvim",
+    version = "*", -- Pin to GitHub releases
+    opts = {
+      fallback_themes = {
+        "catppuccin-frappe",
+        "catppuccin-macchiato",
+        "tokyonight",
+        "kanagawa",
+        "monochrome",
+        "default",
+      },
     },
+    -- dev = true, -- Use local codebase
   },
-  --dev = true,
 }
