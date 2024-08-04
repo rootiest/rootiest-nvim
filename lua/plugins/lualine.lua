b36d8622f7d3de2ede7c456ed8951627b2319320
rootiest <chris@rootiest.com>
2024-08-03 22:43:43 -0400

    ♻️  refactor(lualine): refactor lualine configuration
    Initiate lualine configuration with lualine plugin

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,214 @@
+-- -----------------------------------------------------------------------------
+-- -------------------------------- LUA-LINE -----------------------------------
+-- -----------------------------------------------------------------------------
+
+-- Function to convert RGB to hexadecimal
+local function rgb_to_hex(rgb)
+  return string.format("#%02x%02x%02x", rgb[1], rgb[2], rgb[3])
+end
+
+local function get_fg_color(hlgroup)
+  local hl = vim.api.nvim_get_hl(0, { name = hlgroup, link = false })
+  local fg = hl.fg
+  if fg then
+    return rgb_to_hex({
+      bit.rshift(bit.band(fg, 0xFF0000), 16),
+      bit.rshift(bit.band(fg, 0x00FF00), 8),
+      bit.band(fg, 0x0000FF),
+    })
+  end
+  return nil
+end
+
+return {
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  init = function()
+    vim.g.lualine_laststatus = vim.o.laststatus
+    if vim.fn.argc(-1) > 0 then
+      -- set an empty statusline till lualine loads
+      vim.o.statusline = " "
+    else
+      -- hide the statusline on the starter page
+      vim.o.laststatus = 0
+    end
+  end,
+  opts = function()
+    local icons = LazyVim.config.icons
+
+    -- Require the dependencies
+    local wakatime_stats = require("config.wakatime_stats")
+    local music_stats = require("config.music_stats")
+
+    vim.o.laststatus = vim.g.lualine_laststatus
+
+    local opts = {
+      options = {
+        theme = "auto",
+        globalstatus = vim.o.laststatus == 3,
+        disabled_filetypes = {
+          statusline = { "dashboard", "alpha", "ministarter" },
+        },
+        section_separators = { left = "", right = "" },
+        component_separators = { left = "", right = "" },
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch" },
+
+        lualine_c = {
+          LazyVim.lualine.root_dir(),
+          {
+            "diagnostics",
+            symbols = {
+              error = icons.diagnostics.Error,
+              warn = icons.diagnostics.Warn,
+              info = icons.diagnostics.Info,
+              hint = icons.diagnostics.Hint,
+            },
+          },
+          {
+            "filetype",
+            icon_only = true,
+            separator = "",
+            padding = { left = 1, right = 0 },
+          },
+          { LazyVim.lualine.pretty_path() },
+        },
+        lualine_x = {
+          -- stylua: ignore
+          {
+            ---@diagnostic disable-next-line: undefined-field
+            function() return require("noice").api.status.command.get() end,
+            ---@diagnostic disable-next-line: undefined-field
+            cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+            color = function() return LazyVim.ui.fg("Statement") end,
+          },
+          -- stylua: ignore
+          {
+            ---@diagnostic disable-next-line: undefined-field
+            function() return require("noice").api.status.mode.get() end,
+            ---@diagnostic disable-next-line: undefined-field
+            cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+            color = function() return LazyVim.ui.fg("Constant") end,
+          },
+          -- stylua: ignore
+          {
+            function() return "  " .. require("dap").status() end,
+            cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
+            color = function() return LazyVim.ui.fg("Debug") end,
+          },
+          -- stylua: ignore
+          {
+            require("lazy.status").updates,
+            cond = require("lazy.status").has_updates,
+            color = function() return LazyVim.ui.fg("Special") end,
+          },
+          {
+            "diff",
+            symbols = {
+              added = icons.git.added,
+              modified = icons.git.modified,
+              removed = icons.git.removed,
+            },
+            source = function()
+              local gitsigns = vim.b.gitsigns_status_dict
+              if gitsigns then
+                return {
+                  added = gitsigns.added,
+                  modified = gitsigns.changed,
+                  removed = gitsigns.removed,
+                }
+              end
+            end,
+          },
+          {
+            function()
+              return wakatime_stats.get_icon_with_text() -- Display the icon and WakaTime data together
+            end,
+            color = function()
+              if wakatime_stats.get_total_minutes() >= 60 then
+                return { fg = get_fg_color("diffAdded") }
+              elseif wakatime_stats.get_total_minutes() >= 30 then
+                return { fg = get_fg_color("diffOldFile") }
+              else
+                return { fg = get_fg_color("diffRemoved") }
+              end
+            end,
+            on_click = function(button)
+              if button == "left" then
+                vim.cmd(":WakaTimeToday")
+              end
+            end,
+          },
+        },
+        lualine_y = {
+          {
+            function()
+              return music_stats.get_icon_with_text() -- Display the icon and current state together
+            end,
+          },
+
+          {
+            function()
+              return vim.fn.wordcount().words
+            end,
+            icon = " ",
+          },
+
+          {
+            "progress",
+            separator = " ",
+            padding = { left = 1, right = 0 },
+          },
+          {
+            "location",
+            padding = { left = 0, right = 1 },
+          },
+          {
+            "selection_count",
+          },
+          {
+            "filesize",
+          },
+        },
+        lualine_z = {
+          {
+            "searchcount",
+            color = "CurSearch",
+            separator = { left = "", right = "" },
+            icon = "󰍉 ",
+          },
+          {
+            function()
+              return "  " .. os.date("%R")
+            end,
+          },
+        },
+      },
+      extensions = { "neo-tree", "lazy" },
+    }
+
+    -- do not add trouble symbols if aerial is enabled
+    -- And allow it to be overriden for some buffer types (see autocmds)
+    if vim.g.trouble_lualine and LazyVim.has("trouble.nvim") then
+      local trouble = require("trouble")
+      local symbols = trouble.statusline({
+        mode = "symbols",
+        groups = {},
+        title = false,
+        filter = { range = true },
+        format = "{kind_icon}{symbol.name:Normal}",
+        hl_group = "lualine_c_normal",
+      })
+      table.insert(opts.sections.lualine_c, {
+        symbols and symbols.get,
+        cond = function()
+          return vim.b.trouble_lualine ~= false and symbols.has()
+        end,
+      })
+    end
+
+    return opts
+  end,
+}
