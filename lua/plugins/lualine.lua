df40a4d5056c2189fc5f0d1ab242d8b2b1e924f4
rootiest <chris@rootiest.com>
2024-08-10 07:35:16 -0400

    🐛 fix(lualine): wrap in curlies
    Make sure to wrap each component in curly braces for consistency.

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -83,16 +83,18 @@ return {
               modified = icons.git.modified,
               removed = icons.git.removed,
             },
-            source = function()
-              local gitsigns = vim.b.gitsigns_status_dict
-              if gitsigns then
-                return {
-                  added = gitsigns.added,
-                  modified = gitsigns.changed,
-                  removed = gitsigns.removed,
-                }
-              end
-            end,
+            {
+              source = function()
+                local gitsigns = vim.b.gitsigns_status_dict
+                if gitsigns then
+                  return {
+                    added = gitsigns.added,
+                    modified = gitsigns.changed,
+                    removed = gitsigns.removed,
+                  }
+                end
+              end,
+            },
           },
           {
             function()
