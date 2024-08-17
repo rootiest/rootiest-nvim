27e429df4380dc79271ffacc955a5603ee8cbd0c
rootiest <chris@rootiest.com>
2024-08-17 00:39:10 -0400

    🧹 chore(lazy): fix formatting
    fix formatting in lazy bootstrap

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -4,14 +4,14 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
-	-- stylua: ignore
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", lazypath
-	})
+  -- stylua: ignore
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", lazypath
+  })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
