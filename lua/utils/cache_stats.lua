0650f4c430b80a2bf623611446e793c62ed046ab
rootiest <chris@rootiest.com>
2024-08-23 10:01:01 -0400

    refactor(cache): use vim.fs.joinpath for constructing cache script path

--- lua/utils/cache_stats.lua
+++ lua/utils/cache_stats.lua
@@ -4,8 +4,10 @@
 
 local M = {}
 
+-- Path to the cache script
 local config_dir = vim.fn.stdpath("config")
-local cache_script = config_dir .. "/scripts/update_cache.sh"
+---@diagnostic disable-next-line: param-type-mismatch
+local cache_script = vim.fs.joinpath(config_dir, "/scripts/update_cache.sh")
 
 -- Flag to check if the script has already been run
 local script_run_once = false
@@ -13,7 +15,9 @@ local script_run_once = false
 -- Function to run the cache update script using vim.system
 function M.setup_script()
   if not script_run_once then
+    -- Run the cache script
     vim.system({ cache_script }, { detach = true }, function() end)
+    -- Prevent the script from running again
     script_run_once = true
   end
 end
