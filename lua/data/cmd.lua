82ae6922e05f8a3d933ea0dba3eea261d46c78c1
rootiest <chris@rootiest.com>
2024-08-23 09:56:26 -0400

    ♻️  refactor(cmd): move cmd options to a single file
    Plugins can reference their cmd table from the data.cmd file

---
+++ lua/data/cmd.lua
@@ -0,0 +1,80 @@
+--          ╭─────────────────────────────────────────────────────────╮
+--          │                        CMD DATA                         │
+--          ╰─────────────────────────────────────────────────────────╯
+
+local M = {}
+
+M.autosave = {
+  "ASToggle",
+}
+
+M.codesnap = {
+  "CodeSnap",
+  "CodeSnapSave",
+  "CodeSnapHighlight",
+  "CodeSnapASCII",
+}
+
+M.diffview = {
+  "DiffviewOpen",
+  "DiffviewClose",
+  "DiffviewToggleFiles",
+  "DiffviewFocusFiles",
+  "DiffviewRefresh",
+  "DiffviewFileHistory",
+  "DiffviewLog",
+}
+
+M.fugit = {
+  "Fugit2",
+  "Fugit2Diff",
+  "Fugit2Graph",
+}
+
+M.gist = {
+  "GistCreate",
+  "GistCreateFromFile",
+  "GistsList",
+}
+
+M.gx = {
+  "Browse",
+}
+
+M.kitty_scrollback = {
+  "KittyScrollbackGenerateKittens",
+  "KittyScrollbackCheckHealth",
+}
+
+M.lazygit = {
+  "LazyGit",
+  "LazyGitConfig",
+  "LazyGitCurrentFile",
+  "LazyGitFilter",
+  "LazyGitFilterCurrentFile",
+}
+
+M.nekifoch = "Nekifoch"
+
+M.thanks = {
+  "ThanksAll",
+  "ThanksGithubAuth",
+  "ThanksGithubLogout",
+  "ThanksClearCache",
+}
+
+M.gitlinker = {
+  "GitLink",
+}
+
+M.qalc = {
+  "Qalc",
+  "QalcAttach",
+  "QalcYank",
+}
+
+M.ripsub = {
+  "RipSubstitute",
+}
+
+return M
