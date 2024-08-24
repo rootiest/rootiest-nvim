ef0dbf8962cc806057e5843a71d96bb453cd2423
rootiest <chris@rootiest.com>
2024-08-24 16:31:22 -0400

    refactor: consolidate data module imports into init.lua

---
+++ lua/data/init.lua
@@ -0,0 +1,13 @@
+local keys = require("data.keys")
+local types = require("data.types")
+local func = require("data.func")
+local cmd = require("data.cmd")
+local dash = require("data.dash")
+
+return {
+  keys = keys,
+  types = types,
+  func = func,
+  cmd = cmd,
+  dash = dash,
+}
