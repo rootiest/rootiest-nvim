b6445deed3345ad2725d7c16dc13ef0b419dbeab
rootiest <chris@rootiest.com>
2024-08-28 06:37:44 -0400

    feat(dap): add integration with Neotest for various languages

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -8,4 +8,31 @@ return {
   { -- DAP Neovim Lua Adapter
     import = "lazyvim.plugins.extras.dap.nlua",
   },
+  { -- NeoTest
+    import = "lazyvim.plugins.extras.test.core",
+  },
+  { "nvim-neotest/neotest-plenary" },
+  {
+    "nvim-neotest/neotest",
+    opts = {
+      adapters = {
+        "neotest-plenary",
+        "neotest-python",
+        "neotest-vim-test",
+        "neotest-minitest",
+        "neotest-bash",
+      },
+    },
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "zidhuss/neotest-minitest",
+      "rcasia/neotest-bash",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+  },
+  { -- Profiling
+    "stevearc/profile.nvim",
+  },
 }
