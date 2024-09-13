2db14ae76ad047ee93e3734c565d8e7138e4d33e
rootiest <chris@rootiest.com>
2024-09-13 09:20:39 -0400

    feat: introduce command to update Neovim via nvim-updater

---
+++ lua/plugins/nvim_updater.lua
@@ -0,0 +1,24 @@
+return { -- Neovim Updater
+  "rootiest/nvim-updater.nvim",
+  config = function()
+    require("nvim_updater").setup({})
+  end,
+  cmd = {
+    "UpdateNeovim",
+  },
+  keys = {
+    {
+      "<Leader>quU",
+      ":UpdateNeovim<CR>",
+      desc = "Update Neovim",
+    },
+    {
+      "<Leader>quD",
+      function()
+        require("nvim_updater").update_neovim({ build_type = "Debug" })
+      end,
+      desc = "Debug Build Neovim",
+    },
+  },
+  dev = true,
+}
