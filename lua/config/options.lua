f9b9d23f9a6327f52091b218f08f97f9903b3584
rootiest <chris@rootiest.com>
2024-09-16 09:46:29 -0400

    feat: add nvim-remote support to streamline git operations

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -136,3 +136,8 @@ vim.g.completion_borders   = "rounded" ---@type string Options: [round|sharp|fla
 --  ━━━━━━━━━━━━━━━━━━━━━━━━ Post-Config Functions ━━━━━━━━━━━━━━━━━━━━━━━━
 -- Setup blinky cursor
 require("utils.blinky").setup(vim.g.blinky)
+
+-- Setup nvim-remote
+if vim.fn.executable("nvr") == 1 then
+  vim.env.GIT_EDITOR = "nvr -cc split --remote-wait +'set bufhidden=wipe'"
+end
