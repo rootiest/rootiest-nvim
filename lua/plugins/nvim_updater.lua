5711b8ed335780cb47f45ec9a409b994453dc16b
rootiest <chris@rootiest.com>
2024-09-21 10:32:52 -0400

    fix: change verbosity setting to false in nvim-updater configuration

--- lua/plugins/nvim_updater.lua
+++ lua/plugins/nvim_updater.lua
@@ -3,7 +3,7 @@ local data = require("data")
 return { -- Neovim Updater
   "rootiest/nvim-updater.nvim",
   opts = {
-    verbose = true,
+    verbose = false,
     check_for_updates = true,
     update_interval = (60 * 60) * 6, -- 6 hours
     default_keymaps = false,
