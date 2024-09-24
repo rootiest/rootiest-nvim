9e69a7d36a8faa4ab0b20636ff2b54966f442f0e
rootiest <chris@rootiest.com>
2024-09-24 14:51:25 -0400

    feat: add img-clip plugin with custom configuration

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -173,6 +173,21 @@ return {
   { -- Timer
     "alex-popov-tech/timer.nvim",
   },
+  { -- Image-Clip
+    "HakonHarnes/img-clip.nvim",
+    event = "VeryLazy",
+    opts = {
+      default = {
+        embed_image_as_base64 = false,
+        prompt_for_file_name = false,
+        drag_and_drop = {
+          insert_mode = true,
+        },
+        -- required for Windows users
+        use_absolute_path = true,
+      },
+    },
+  },
   { -- Multicursor
     "jake-stewart/multicursor.nvim",
     branch = "1.0",
