7ef39df68fc3ab2b929a836619f00888b42bb999
rootiest <chris@rootiest.com>
2024-10-11 12:48:14 -0400

    refactor: remove redundant Git conflict handling logic

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -9,50 +9,50 @@ local autocmd = vim.api.nvim_create_autocmd
 -- GIT CONFLICT MARKERS
 -- if there are conflicts, jump to first conflict, highlight conflict markers,
 -- and disable diagnostics (simplified version of `git-conflict.nvim`)
-autogrp("GitConflictMarkers", { clear = true })
-autocmd({ "BufEnter", "FocusGained" }, {
-  group = "GitConflictMarkers",
-  callback = function(ctx)
-    local hlgroup = "DiagnosticVirtualTextInfo" -- CONFIG
-
-    if not vim.api.nvim_buf_is_valid(ctx.buf) then
-      return
-    end
-    vim.system(
-      { "git", "diff", "--check", "--", vim.api.nvim_buf_get_name(ctx.buf) },
-      {},
-      vim.schedule_wrap(function(out)
-        local noConflicts = out.code == 0
-        local noGitRepo =
-          vim.startswith(out.stdout, "warning: Not a git repository")
-        if noConflicts or noGitRepo then
-          return
-        end
-
-        local ns = vim.api.nvim_create_namespace("conflictMarkers")
-        local firstConflict
-        for conflictLnum in out.stdout:gmatch("(%d+): leftover conflict marker") do
-          local lnum = tonumber(conflictLnum)
-          vim.api.nvim_buf_add_highlight(ctx.buf, ns, hlgroup, lnum - 1, 0, -1)
-          if not firstConflict then
-            firstConflict = lnum
-          end
-        end
-        if not firstConflict then
-          return
-        end
-
-        vim.api.nvim_win_set_cursor(0, { firstConflict, 0 })
-        vim.diagnostic.enable(false, { bufnr = ctx.buf })
-        vim.notify_once(
-          "Conflict markers found.",
-          nil,
-          { title = "Git Conflicts" }
-        )
-      end)
-    )
-  end,
-})
+-- autogrp("GitConflictMarkers", { clear = true })
+-- autocmd({ "BufEnter", "FocusGained" }, {
+--   group = "GitConflictMarkers",
+--   callback = function(ctx)
+--     local hlgroup = "DiagnosticVirtualTextInfo" -- CONFIG
+--
+--     if not vim.api.nvim_buf_is_valid(ctx.buf) then
+--       return
+--     end
+--     vim.system(
+--       { "git", "diff", "--check", "--", vim.api.nvim_buf_get_name(ctx.buf) },
+--       {},
+--       vim.schedule_wrap(function(out)
+--         local noConflicts = out.code == 0
+--         local noGitRepo =
+--           vim.startswith(out.stdout, "warning: Not a git repository")
+--         if noConflicts or noGitRepo then
+--           return
+--         end
+--
+--         local ns = vim.api.nvim_create_namespace("conflictMarkers")
+--         local firstConflict
+--         for conflictLnum in out.stdout:gmatch("(%d+): leftover conflict marker") do
+--           local lnum = tonumber(conflictLnum)
+--           vim.api.nvim_buf_add_highlight(ctx.buf, ns, hlgroup, lnum - 1, 0, -1)
+--           if not firstConflict then
+--             firstConflict = lnum
+--           end
+--         end
+--         if not firstConflict then
+--           return
+--         end
+--
+--         vim.api.nvim_win_set_cursor(0, { firstConflict, 0 })
+--         vim.diagnostic.enable(false, { bufnr = ctx.buf })
+--         vim.notify_once(
+--           "Conflict markers found.",
+--           nil,
+--           { title = "Git Conflicts" }
+--         )
+--       end)
+--     )
+--   end,
+-- })
 
 -- LazyGit root detection
 if pcall(require, "lazygit.utils") then
@@ -63,18 +63,6 @@ if pcall(require, "lazygit.utils") then
     end,
   })
 end
-
--- TodoFzfLua command override to use Telescope
--- when fzf-lua is not installed
-autogrp("TodoFzfLua", { clear = true })
-autocmd({ "BufEnter" }, {
-  group = "TodoFzfLua",
-  callback = function()
-    if not pcall(require, "fzf-lua") then
-      vim.cmd([[command! -nargs=* TodoFzfLua :TodoTelescope]])
-    end
-  end,
-})
 
 --  ━━━━━━━━━━━━━━━━━━━━━━━ Set up Qalc keymappings ━━━━━━━━━━━━━━━━━━━━━━━
 if pcall(require, "qalc") then
@@ -119,3 +107,23 @@ end
 local load_highlight = require("utils.highlight")
 load_highlight.setup_autocommands()
 load_highlight.setup_dashboard_highlight()
+
+--  ━━━━━━━━━━━━━━━━━━━━━━━━━━ Set up nvim_exec ━━━━━━━━━━━━━━━━━━━━━━━
+
+-- Function to check and execute the NVIM_EXEC environment variable
+local function execute_nvim_exec()
+  local exec_command = os.getenv("NVIM_EXEC")
+
+  -- If NVIM_EXEC is set, execute the command
+  if exec_command then
+    vim.cmd(exec_command)
+  end
+end
+
+-- Define an autogroup for the autocmd
+vim.api.nvim_create_augroup("NvimExec", { clear = true })
+-- Define an autocmd to run the function at startup
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = "NvimExec",
+  callback = execute_nvim_exec,
+})
