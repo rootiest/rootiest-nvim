8a36aa360265936266ebb7dc92f064bf802d8eef
rootiest <chris@rootiest.com>
2024-10-11 12:48:04 -0400

    feat: add .styluaignore to exclude options.lua from formatting

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,9 +5,8 @@
 --          ╭─────────────────────────────────────────────────────────╮
 --          │                         Options                         │
 --          ╰─────────────────────────────────────────────────────────╯
--- stylua: ignore start
 
---  ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ Key Options ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+--  ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ KEYS ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 -- Set the mapleader variable to the space key
 vim.g.mapleader            = " "       ---@type string Options: <leader>
@@ -26,13 +25,19 @@ vim.g.loaded_ruby_provider = 0         ---@type integer Options: <0|1>
 -- Prefer basedpyright for python
 vim.g.lazyvim_python_lsp   = "pyright" ---@type string Options: [python lsp]
 -- Open all folds by default
+
+--  ━━━━━━━━━━━━━━━━━━━━━━━━━━━ FORMAT AND SAVE ━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
 vim.opt.foldlevel          = 99        ---@type integer Options: <0-99>
 -- Auto-format
 vim.g.autoformat           = true      ---@type boolean Options: <true|false>
 -- Auto-save
 vim.g.autosave             = true      ---@type boolean Options: <true|false>
+-- Enable encouraging feedback
+vim.g.encourage_me         = true     ---@type boolean Options:  <true|false>
 
---  ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ROOTIEST ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+--  ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ MISCELLANEOUS ━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 -- Use WakaTime for code stats
 vim.g.usewakatime     =  true          ---@type boolean Options:  <true|false>
@@ -48,18 +53,38 @@ vim.g.uselazygit      =  true          ---@type boolean Options:  <true|false>
 vim.g.ignore_no_lazy  =  false         ---@type boolean Options:  <true|false>
 -- Enable codesnap for code screenshots
 vim.g.codesnap        =  true          ---@type boolean Options:  <true|false>
--- Enable encouraging feedback
-vim.g.encourage_me    =  true          ---@type boolean Options:  <true|false>
 -- Enable Auto-hiding cursorline
 vim.g.auto_cursorline =  true          ---@type boolean Options: <true|false>
+-- Enable Smart Scrolloff
+vim.g.smart_scrolloff =  false         ---@type boolean Options: <true|false>
+-- Enable Dead Column
+vim.g.dead_column     =  false         ---@type boolean Options: <true|false>
 -- Animate blinky cursor
 vim.g.blinky          =  true          ---@type boolean Options: <true|false>
 -- Use experimental cmp performance fork
-vim.g.cmp_performance_enabled = false  ---@type boolean Options: <true|false>
+vim.g.cmp_performance_enabled = true   ---@type boolean Options: <true|false>
 -- Use dev mode for rootiest plugins
-vim.g.rootiest_dev    =  false         ---@type boolean Options: <true|false>
+vim.g.rootiest_dev    =  true          ---@type boolean Options: <true|false>
+
+--  ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ PICKER ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+-- Select default picker
+vim.g.lazyvim_picker = "fzf"           ---@type string Options: [picker]
+--        ╭───────────────────────────╮                auto
+--        │                           │                telescope
+--        │          Pickers:         │                fzf
+--        │      Choose a picker      │
+--        │      from the list       │
+--        │                           │
+--        ╰───────────────────────────╯
+
+-- Use the Telescope plugin
+vim.g.use_telescope   =  true          ---@type boolean Options: <true|false>
+-- Use the Fzf plugin
+vim.g.use_fzf_lua     =  true          ---@type boolean Options: <true|false>
 
 --  ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ AI TOOL ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
 --  Configure the AI completion provider
 vim.g.aitool          = "neocodeium"   ---@type string Options: [ai tool]
 --        ╭───────────────────────────╮                neocodeium
@@ -77,6 +102,7 @@ vim.g.useavante       =  true           ---@type boolean Options:  <true|false>
 vim.g.usegpai         =  false          ---@type boolean Options:  <true|false>
 
 --  ━━━━━━━━━━━━━━━━━━━━━━━━━━━━ STATUS COLUMN ━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
 vim.g.statuscolumn    = "native"        ---@type string Options: [statuscolumn]
 --        ╭───────────────────────────╮                barsNlines
 --        │                           │                native
@@ -87,6 +113,7 @@ vim.g.statuscolumn    = "native"        ---@type string Options: [statuscolumn]
 --        ╰───────────────────────────╯
 
 --  ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ BUFFER LINE ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
 vim.g.tabline         = "none"         ---@type string Options: [tabline]
 --        ╭───────────────────────────╮                bufferline
 --        │                           │                barsNlines
@@ -97,6 +124,7 @@ vim.g.tabline         = "none"         ---@type string Options: [tabline]
 --        ╰───────────────────────────╯
 
 --  ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ DASHBOARD ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
 vim.g.dashboard       = "alpha"        ---@type string Options: [dashboard]
 --        ╭───────────────────────────╮                alpha
 --        │                           │                nvim-dashboard
@@ -112,6 +140,7 @@ vim.g.dash_logo = "neovim.txt"    ---@type string Options: [dash logo]
 vim.g.DashboardHeaderColor = "#88fc9a"---@type string Options: [hex color]
 
 --  ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ STATUS LINE ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
 vim.g.statusline      = "lualine"      ---@type string Options: [statusline]
 --        ╭───────────────────────────╮                lualine
 --        │                           │                heirline
@@ -146,7 +175,8 @@ vim.g.disable_transparency = true      ---@type boolean Options: <true|false>
 -- Set colorcolumn
 --vim.opt.colorcolumn        = "120"     ---@type string|table Options: [column]
 
---  ━━━━━━━━━━━━━━━━━━━━━━━━━ Tabs and Indentation ━━━━━━━━━━━━━━━━━━━━━━━━
+--  ━━━━━━━━━━━━━━━━━━━━━━━━━ TABS AND INDENTATION ━━━━━━━━━━━━━━━━━━━━━━━━
+
 --  Define characters for tabs, indents, and scopes
 --  Option [2] will override [1] if both are defined
 
@@ -167,18 +197,16 @@ vim.g.scope_char_name      = "scope"   ---@type string Options: [scope char]
 -- Multi-character cycling configurations are not possible with this option
 -- Multi-character strings are not possible with any of the options
 --  Tab character
--- vim.g.tab_char          = "󰌒"       ---@type string Options: [tab char]
+vim.g.tab_char          = " "       ---@type string Options: [tab char]
 -- -- Indent character
--- vim.g.indent_char       = "¶"       ---@type string Options: [indent char]
+vim.g.indent_char       = " "       ---@type string Options: [indent char]
 -- Scope character
--- vim.g.scope_char        = ""       ---@type string Options: [scope char]
+vim.g.scope_char        = "│"       ---@type string Options: [scope char]
 
 --  ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ Completion ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 -- Cmp Window Border
 vim.g.completion_borders   = "rounded" ---@type string Options: [round|sharp|flat]
-
--- stylua: ignore end
 
 --  ━━━━━━━━━━━━━━━━━━━━━━━━ Post-Config Functions ━━━━━━━━━━━━━━━━━━━━━━━━
 
@@ -189,3 +217,4 @@ require("utils.blinky").setup(vim.g.blinky)
 if vim.fn.executable("nvr") == 1 then
   vim.env.GIT_EDITOR = "nvr -cc split --remote-wait +'set bufhidden=wipe'"
 end
+-- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
