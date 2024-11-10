50cebdbddd797d8794cdf3819eeb969db45e38a0
rootiest <chris@rootiest.com>
2024-11-10 16:15:04 -0500

    feat: add support for termux environment detection

--- lua/config/postopts.lua
+++ lua/config/postopts.lua
@@ -7,3 +7,13 @@ require("utils.blinky").setup(vim.g.blinky)
 if vim.fn.executable("nvr") == 1 then
   vim.env.GIT_EDITOR = "nvr -cc split --remote-wait +'set bufhidden=wipe'"
 end
+
+-- Check for termux
+local prefix = vim.env.PREFIX
+
+-- Check if prefix contains "com.termux"
+if prefix ~= nil then
+  if prefix:match("com.termux") then
+    vim.g.useimage = false
+  end
+end
