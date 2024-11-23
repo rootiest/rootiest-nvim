4ae59dee6c1574cd603fb93c45e6f61166fd65bd
rootiest <chris@rootiest.com>
2024-11-23 14:25:49 -0500

    feat: support latex in treesitter configurations

--- lua/data/types.lua
+++ lua/data/types.lua
@@ -237,6 +237,7 @@ M.general = {
   ft = {
     'help',
     'dashboard',
+    'snacks_dashboard',
     'neorg',
   },
 }
@@ -1838,6 +1839,7 @@ M.treesitter = {
       'jsdoc',
       'json',
       'jsonc',
+      'latex',
       'lua',
       'luadoc',
       'luap',
@@ -1910,21 +1912,22 @@ M.zen = {
     wezterm = {
       enabled = require('data.func').is_wezterm(),
       -- can be either an absolute font size or the number of incremental steps
-      font = '+1', -- (10% increase per step)
+      font = '+2', -- (10% increase per step)
     },
     neovide = {
       enabled = require('data.func').is_neovide(),
       -- Will multiply the current scale factor by this number
       scale = 1.2,
       -- disable the Neovide animations while in Zen mode
-      disable_animations = {
-        neovide_animation_length = 0,
-        neovide_cursor_animate_command_line = false,
-        neovide_scroll_animation_length = 0,
-        neovide_position_animation_length = 0,
-        neovide_cursor_animation_length = 0,
-        neovide_cursor_vfx_mode = '',
-      },
+      disable_animations = false,
+      -- disable_animations = {
+      --   neovide_animation_length = 0,
+      --   neovide_cursor_animate_command_line = false,
+      --   neovide_scroll_animation_length = 0,
+      --   neovide_position_animation_length = 0,
+      --   neovide_cursor_animation_length = 0,
+      --   neovide_cursor_vfx_mode = '',
+      -- },
     },
   },
 }
--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -39,6 +39,7 @@ return {
       library = {
         { path = 'luvit-meta/library', words = { 'vim%.uv' } },
         { path = 'LazyVim', words = { 'LazyVim' } },
+        { path = 'snacks.nvim', words = { 'Snacks' } },
         { path = 'lazy.nvim', words = { 'LazyVim' } },
         -- Load the wezterm types when the `wezterm` module is required
         -- Needs `justinsgithub/wezterm-types` to be installed
