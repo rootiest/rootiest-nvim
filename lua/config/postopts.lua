4f62a6b91c13449ae3592781b1a94029021fc14c
rootiest <chris@rootiest.com>
2024-11-24 03:19:35 -0500

    refactor: encapsulate setup logic into a dedicated function

--- lua/config/postopts.lua
+++ lua/config/postopts.lua
@@ -1,31 +1,37 @@
 --  ━━━━━━━━━━━━━━━━━━━━━━━━ Post-Config Functions ━━━━━━━━━━━━━━━━━━━━━━━━
 
--- Setup blinky cursor
-require('utils.blinky').setup(vim.g.blinky)
+--- Function to setup post-opts
+local function setup()
+  -- Setup blinky cursor
+  require('utils.blinky').setup(vim.g.blinky)
 
--- Setup nvim-remote
-if vim.fn.executable('nvr') == 1 then
-  vim.env.GIT_EDITOR = "nvr -cc split --remote-wait +'set bufhidden=wipe'"
-end
+  -- Setup nvim-remote
+  if vim.fn.executable('nvr') == 1 then
+    vim.env.GIT_EDITOR = "nvr -cc split --remote-wait +'set bufhidden=wipe'"
+  end
 
--- Check for termux
-local prefix = vim.env.PREFIX
+  -- Check for termux
+  local prefix = vim.env.PREFIX
 
--- Check if prefix contains "com.termux"
-if prefix ~= nil then
-  if prefix:match('com.termux') then
+  -- Check if prefix contains "com.termux"
+  if prefix ~= nil then
+    if prefix:match('com.termux') then
+      vim.g.useimage = false
+      vim.g.is_termux = true
+      vim.g.useavante = false
+    end
+  end
+
+  -- Define intel_arch
+  local intel_arch = { x64 = true, x86 = true }
+  local cur_arch = require('data.func').get_system_arch()
+
+  -- Check the platform and disable incompatible plugins
+  if not intel_arch[cur_arch] then
     vim.g.useimage = false
-    vim.g.is_termux = true
     vim.g.useavante = false
   end
 end
 
--- Define intel_arch
-local intel_arch = { x64 = true, x86 = true }
-local cur_arch = require('data.func').get_system_arch()
-
--- Check the platform and disable incompatible plugins
-if not intel_arch[cur_arch] then
-  vim.g.useimage = false
-  vim.g.useavante = false
-end
+-- Run Setup function
+setup()
