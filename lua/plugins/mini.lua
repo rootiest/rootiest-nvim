1aa52c44a54bae2cafc661bd6ebe54e011659afb
rootiest <chris@rootiest.com>
2025-02-08 21:28:38 -0500

    move opts/init to types.lua

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -50,21 +50,7 @@ return {
   { -- mini.icons
     'echasnovski/mini.icons',
     lazy = true,
-    opts = {
-      file = {
-        ['.keep'] = { glyph = '󰊢', hl = 'MiniIconsGrey' },
-        ['devcontainer.json'] = { glyph = '', hl = 'MiniIconsAzure' },
-      },
-      filetype = {
-        dotenv = { glyph = '', hl = 'MiniIconsYellow' },
-      },
-    },
-    init = function()
-      ---@diagnostic disable-next-line: duplicate-set-field
-      package.preload['nvim-web-devicons'] = function()
-        require('mini.icons').mock_nvim_web_devicons()
-        return package.loaded['nvim-web-devicons']
-      end
-    end,
+    opts = require('data.types').miniicons.opts,
+    init = require('data.types').miniicons.init,
   },
 }
